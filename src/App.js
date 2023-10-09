f88b185d143c91f374a6d723031725ef378556c5
HollyMaguire <holly@Hollys-MacBook-Pro.local>
2023-10-08 18:36:13 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {Icon} from 'react-icons-kit';
+import {eyeOff} from 'react-icons-kit/feather/eyeOff';
+import {eye} from 'react-icons-kit/feather/eye'
+import $ from 'jquery';
 
-function App() {
+export const validLogin = (password, email) => validPassword(password) && validEmail(email)
+
+export const validPassword = (password) => password.length > 0
+
+export const validEmail = (email) => /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)
+
+export function App() {
+const [email, setEmail] = useState('')
+const [password, setPassword] = useState('')
+const [type, setType] = useState('password')
+const [icon, setIcon] = useState(eyeOff)
+
+const login = () => {
+  if(!validLogin(password, email)) {
+    alert("Invalid login")
+    return
+  }
+
+  $.ajax({
+    url: "https://reqres.in/api/login",
+    type: "POST",
+    data: {
+        password,
+        email
+    },
+    success: function(){
+        alert("login successful")
+    },
+    error: function(response){
+      alert(response.responseJSON.error)
+  }
+
+})
+
+}
+
+const handleToggle = () => {
+   if (type==='password'){
+      setIcon(eye);
+      setType('text')
+   } else {
+      setIcon(eyeOff)
+      setType('password')
+   }
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="login-App">
+      <div className="login-form">
+        <form>
+          <div className="login-header">
+          <header>
+            Log In 
+          </header>
+          </div>
+          <div className= "email">
+            <input
+              type="email"
+              name="email"
+              placeholder="myEmail@email.com" 
+              id="email" 
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              label="Email Address" 
+              
+              autoFocus={true}/>
+            </div>  
+          <div className= "password">
+            <input 
+            type={type}
+            name="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            label="Password" 
+            autoFocus={true} />
+              <span className="show-password" onClick={handleToggle}>
+                    <Icon icon={icon} size={15}/>
+                </span>
+            </div>
+            
+        </form>
+        <button 
+        className="submit" 
+        onClick={login} >
+              Submit
+        </button>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,35 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App, {validPassword, validEmail, validLogin} from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('rejects short password', () => {
+  let password = ''
+  expect(
+    validPassword(password)).toBe(false);
+})
+;
+
+test('accepts password with length greater than 0', () => {
+  let password = 'a'
+  expect(
+    validPassword(password)).toBe(true);
 });
+
+test('rejects badly formated email', () => {
+  let email = 'athing.com'
+  expect(
+    validEmail(email)).toBe(false);
+});
+
+test('accepts correctly formatted email', () => {
+  let email = 'stuff@athing.com'
+  expect(
+    validEmail(email)).toBe(true);
+});
+
+test('accepts a correctly formatted email and password', () => {
+  let email = 'stuff@athing.com'
+  let password = "password"
+  expect(
+    validLogin(password, email)).toBe(true);
+});
+
